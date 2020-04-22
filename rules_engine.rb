43cf76611636de640c53d56fa0cd596f58f1da94
Shelby El-rassi <60080830+Shelby219@users.noreply.github.com>
2020-04-22 15:12:20 +1000

    Rules Engine file and testing Update
    I was able to write some proper tests and able to test the rules engine module method for a draw, the module method for the player winning, and the module method for the computer winning.

--- rules_engine.rb
+++ rules_engine.rb
@@ -1,14 +1,91 @@
 module RulesEngine
 	module_function
 	
-	def who_won(move1, move2)
-		# Returns either :first, :second or :draw
-		if move1 == move2 
+	# def who_won(move1, move2)
+	# 	# Returns either :first, :second or :draw
+	# 	if move1 == move2 
+	# 		:draw
+	# 	elsif  move1 == :scissors and move2 == :paper 
+	# 		:first
+	# 	end
+	# end
+
+	def round_draw(player_move, computer_move)
+		if player_move == computer_move
 			:draw
-		elsif move1 == :paper and move2 == :scissors
-			:second
 		end
 	end
-	
-	
+
+	def player_win(player_move, computer_move)
+		if player_move == :scissors and computer_move == :paper 
+			:first 
+		#puts "#{player_move} cuts #{computer_move}"
+		elsif player_move == :paper and computer_move == :rock 
+			:first	
+		# 	puts "#{player_move} covers #{computer_move}"
+		elsif player_move == :rock and computer_move == :lizard 
+			:first	
+		# 	puts "#{player_move} crushes #{computer_move}"
+		elsif player_move == :lizard and computer_move == :spock 
+			:first	
+			# 	puts "#{player_move} poisons #{computer_move}"
+		elsif player_move == :spock and computer_move == :scissors 
+			:first	
+		# 	puts "#{player_move} smashes #{computer_move}"
+		elsif player_move == :scissors and computer_move == :lizard 
+			:first	
+		# 	puts "#{player_move} decapitate #{computer_move}"
+		elsif player_move == :lizard and computer_move == :paper 
+			:first	
+		# 	puts "#{player_move} eats #{computer_move}"
+		elsif player_move == :paper and computer_move == :spock 
+			:first	
+		# 	puts "#{player_move} disproves #{computer_move}"
+		elsif player_move == :spock and computer_move == :rock 
+			:first	
+		# 	puts "#{player_move} vaporizes #{computer_move}"
+		elsif player_move == :rock and computer_move == :scissors 
+			:first	
+		# 	puts "#{player_move} crushes #{computer_move}"
+		end
+	end
+
+	def computer_win(computer_move, player_move)
+		if computer_move == :scissors and player_move == :paper 
+			:first	
+		#   puts "#{computer_move} cuts #{player_move}"
+		elsif computer_move == :paper and player_move == :rock 
+			:first	
+	# 		puts "#{computer_move} covers #{player_move}"
+		elsif computer_move == :rock and player_move == :lizard  
+			:first	
+	# 		puts "#{computer_move} crushes #{player_move}"
+		elsif computer_move == :lizard and player_move == :spock 
+			:first	
+	# 		puts "#{computer_move} poisons #{player_move}"
+		elsif computer_move == :spock and computer_move == :scissors 
+			:first	
+	# 		puts "#{computer_move} smashes #{player_move}"
+		elsif computer_move == :scissors and player_move == :lizard 
+			:first	
+	# 		puts "#{computer_move} decapitate #{player_move}"
+		elsif computer_move == :lizard and player_move == :paper 
+			:first	
+	# 		puts "#{computer_move} eats #{player_move}"
+		elsif computer_move == :paper and player_move == :spock 
+			:first	
+	# 		puts "#{computer_move} disproves #{player_move}"
+		elsif computer_move== :spock and player_move == :rock 
+			:first	
+	# 		puts "#{computer_move} vaporizes #{player_move}"
+		elsif computer_move == :rock and player_move == :scissors 
+			:first	
+	# 		puts "#{computer_move} crushes #{player_move}"
+		end	
+	end
+
 end
+	 
+
+
+RulesEngine.player_win(:scissors, :paper)
--- rules_engine_test.rb
+++ rules_engine_test.rb
@@ -1,19 +1,36 @@
 require 'test/unit'
-
-require_relative 'rules_engine.rb'
+require_relative './rules_engine.rb'
 
 #test
 
-class GameTest < Test::Unit::TestCase
+class RulesTest < Test::Unit::TestCase
     def test_draw
-        outcome = RulesEngine::who_won(:spock, :spock)
+        # outcome = RulesEngine::who_won(:rock, :rock)
+        # outcome = RulesEngine::who_won(:paper, :paper)
+        # outcome = RulesEngine::who_won(:scissors, :scissors)
+        # outcome = RulesEngine::who_won(:spock, :spock)
+        # outcome = RulesEngine::who_won(:lizard, :lizard)
+        # assert_equal(:draw, outcome)
+        outcome = RulesEngine::round_draw(:rock, :rock)
         assert_equal(:draw, outcome)
     end
     
     def test_win
-        outcome = RulesEngine::who_won(:paper, :scissors)
-        assert_equal(:second, outcome)
+        outcome = RulesEngine::player_win(:scissors, :paper)
+        assert_equal(:first, outcome)
+        outcome = RulesEngine::computer_win(:scissors, :paper)
+        assert_equal(:first, outcome)
     end
 
 end
-    
+
+
+
+        # outcome = RulesEngine::who_won(:paper, :rock)
+        # outcome = RulesEngine::who_won(:rock, :scissors)
+        # outcome = RulesEngine::who_won(:lizard, :spock)
+        # outcome = RulesEngine::who_won(:spock, :scissors)
+        # outcome = RulesEngine::who_won(:scissors, :lizard)
+        # outcome = RulesEngine::who_won(:lizard, :paper)
+        # outcome = RulesEngine::who_won(:paper, :spock)
+        # outcome = RulesEngine::who_won(:rock, :scissors)
