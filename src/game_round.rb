5aaf65c0bd445ca59c5ad93df07cb76e68ff43e2
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-04-25 19:45:31 +1000

    Winner declaration if else statement added
    Added a if else to declare a winner one the score of 5 is reached for either player or computer

--- src/game_round.rb
+++ src/game_round.rb
@@ -5,7 +5,6 @@ require 'io/console'
 require 'tty-prompt'
 
 
-
 require_relative 'weapons.rb'
 require_relative 'rules_engine.rb'
 require_relative 'computer_player.rb'
@@ -15,17 +14,17 @@ require_relative 'computer_player.rb'
 module Player 
     module_function #CURSED BE THIS CODE STATEMENT
     #include RulesEngine
+    attr_accessor :name
 
     def main_player
         begin
          puts "What is your player name?\n\n"
-         @name = gets.strip
-         raise if @name.length > 10
+         @@name = gets.strip.to_s #this is still not updating the instance variable.
+         raise if @@name.length > 10
          rescue StandardError => e
             puts "Invalid name length, must be 10 characters or less\n\n"
             main_player
          end
-        # unless ARGV.empty?
     end
       
     def player_choice
@@ -43,6 +42,12 @@ module Player
       end
     end  
 
+    def final_winner(play, com)
+      return :PWIN if @player_score == @winning_score
+      return :CWIN if @computer_score == @winning_score
+    end
+
+
     
 end
 
@@ -57,22 +62,22 @@ class GameRound
         @player_score = 0
         @computer_score = 0
         @winning_score = 5
-        @name 
+        #@@name = name
     end
 
-    def get_name
-      main_player
-    end
+    # def get_name
+    #   main_player
+    # end
 
     def get_score #This is not working at the moment as well- the scores are not updating
         puts "SCORE: Player - #{@player_score.to_i} Computer - #{@computer_score.to_i}\n\n"
     end 
       
     def play
-      #while @player_score <  @winning_score && @computer_score <  @winning_score
+      while @player_score <  @winning_score && @computer_score <  @winning_score
         player = player_choice
         computer = ComputerPlayer.new.move_shuffle #choose a random option for computer
-         puts "#{@name}  chooses #{player.to_s.downcase}.\n\n"
+         puts "#{@@name} chooses #{player.to_s.downcase}.\n\n"
          puts "Computer chooses #{computer.to_s.downcase}.\n\n"
         case RulesEngine.round_outcome(player, computer)
         when :DRAW 
@@ -80,7 +85,7 @@ class GameRound
           play
         when :PLAYERWIN 
           @player_score += 1
-          puts "#{player.to_s.capitalize} beats #{computer.to_s.downcase}, #{@name} wins the round.\n\n" 
+          puts "#{player.to_s.capitalize} beats #{computer.to_s.downcase}, #{@@name} wins the round.\n\n" 
           #return RulesEngine::player_win(player, computer) #This is not passing this method at the moment
           get_score
         when :COMPUTERWIN 
@@ -89,11 +94,19 @@ class GameRound
           #return RulesEngine::computer_win(computer, player)
           get_score
          end
-         puts "NEXT ROUND!\n\n"
-         puts "Press any any key to continue!\n\n"
-        STDIN.getch  #Waits for user input (Any Key)
-        puts `clear` # Clears screen after
-     #end
+         if @player_score == @winning_score 
+            puts "#{@@name} is the ULTIMATE RPSSL CHAMPION!"
+            break
+         elsif @computer_score == @winning_score 
+            puts "Too bad, the computer remains the ULTIMATE RPSSL CHAMPION!"
+            break
+         else
+            puts "NEXT ROUND!\n\n"
+            puts "Press any any key to continue!\n\n"
+            STDIN.getch  #Waits for user input (Any Key)
+             puts `clear` # Clears screen after
+         end
+     end
     end
 
   
@@ -103,8 +116,6 @@ end
 Player.main_player
 newgame = GameRound.new.play
 
-
-#newgame.get_score
 
 
 
