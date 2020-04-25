4b4a48a2749e5959c5249cd31393f957631a242b
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-04-25 17:20:34 +1000

    Game_round now function as it should
    The game round loop is functioning and working, the loop is stopping at five points. Good decsion to merge the scoreboard in this class to save time at the moment. That is now on the nice to have list along with incorporating the functions from rules engine which have the wording for each weapons win, using beat at the moment. Errors still with the @name instance variable updating. Refactoring...

--- src/game_round.rb
+++ src/game_round.rb
@@ -1,3 +1,11 @@
+require 'tty-cursor'
+require 'tty-screen'
+require 'tty-spinner'
+require 'io/console'
+require 'tty-prompt'
+
+
+
 require_relative 'weapons.rb'
 require_relative 'rules_engine.rb'
 require_relative 'computer_player.rb'
@@ -5,10 +13,24 @@ require_relative 'computer_player.rb'
 
 #this module is a section for the player methods to be held
 module Player 
-    include RulesEngine
+    module_function #CURSED BE THIS CODE STATEMENT
+    #include RulesEngine
+
+    def main_player
+        begin
+         puts "What is your player name?\n\n"
+         @name = gets.strip
+         raise if @name.length > 10
+         rescue StandardError => e
+            puts "Invalid name length, must be 10 characters or less\n\n"
+            main_player
+         end
+        # unless ARGV.empty?
+    end
+      
     def player_choice
      loop do
-       puts "Choose your weapon: Rock (r), Paper (p), Scissors (s), Spock (sp), Lizard (l)"
+       puts "Choose your weapon: Rock (r), Paper (p), Scissors (s), Spock (sp), Lizard (l)\n\n"
        answer = gets.chomp.downcase
        #checking for valid answer
        if Weapons::ENTRY.key?(answer)
@@ -17,9 +39,10 @@ module Player
        #exception handling for invalid entry
     raise if answer != Weapons::PLAYER_ENTRIES
     rescue  StandardError => e
-        puts "That entry is invalid. Please re-nter!"
+        puts "That entry is invalid. Please re-nter!\n\n"
       end
     end  
+
     
 end
 
@@ -27,62 +50,61 @@ class GameRound
     #include ScoreBoard
     include Player
     include RulesEngine
-    attr_accessor :player_score, :computer_score
+    include Weapons
+    attr_accessor :player_score, :computer_score, :name
 	
-	def initialize (player_score = 0, computer_score = 0)
-        @player_score = player_score
-        @computer_score = computer_score
+	def initialize #intialize the variables
+        @player_score = 0
+        @computer_score = 0
         @winning_score = 5
+        @name 
     end
+
+    def get_name
+      main_player
+    end
+
+    def get_score #This is not working at the moment as well- the scores are not updating
+        puts "SCORE: Player - #{@player_score.to_i} Computer - #{@computer_score.to_i}\n\n"
+    end 
       
     def play
       #while @player_score <  @winning_score && @computer_score <  @winning_score
         player = player_choice
         computer = ComputerPlayer.new.move_shuffle #choose a random option for computer
-         puts "Player chooses #{player.to_s.downcase}."
-         puts "Computer chooses #{computer.to_s.downcase}."
-        # RulesEngine.round_outcome(player, computer)
-        # if :DRAW
-        #     puts "Tie, play again!"
-        #    # play
-        # elsif :PLAYERWIN
-        #     RulesEngine.player_win(player, computer) 
-        #     @player_score += 1
-        # elsif :COMPUTERWIN
-        #     return RulesEngine.computer_win(computer, player)
-        #     @computer_score += 1
-        # else
-        #     puts "METHOD ERROR"
-        # end
-      case RulesEngine.round_outcome(player, computer)
-      when :DRAW 
-        puts "Tie, play again!"
-        play
-      when :PLAYERWIN 
-        puts "#{player.to_s.capitalize} beats #{computer.to_s.downcase}, player wins the round." 
-        #return RulesEngine.player_win(player, computer) #This is not passing this method at the moment
-        @player_score += 1
-      when :COMPUTERWIN 
-        puts "#{computer.to_s.capitalize} beats #{player.to_s.downcase}, computer wins the round." 
-        #return RulesEngine.computer_win(computer, player)
-        @computer_score += 1
-      end
+         puts "#{@name}  chooses #{player.to_s.downcase}.\n\n"
+         puts "Computer chooses #{computer.to_s.downcase}.\n\n"
+        case RulesEngine.round_outcome(player, computer)
+        when :DRAW 
+          puts "Tie, play again!\n\n"
+          play
+        when :PLAYERWIN 
+          @player_score += 1
+          puts "#{player.to_s.capitalize} beats #{computer.to_s.downcase}, #{@name} wins the round.\n\n" 
+          #return RulesEngine::player_win(player, computer) #This is not passing this method at the moment
+          get_score
+        when :COMPUTERWIN 
+          @computer_score += 1
+          puts "#{computer.to_s.capitalize} beats #{player.to_s.downcase}, computer wins the round.\n\n" 
+          #return RulesEngine::computer_win(computer, player)
+          get_score
+         end
+         puts "NEXT ROUND!\n\n"
+         puts "Press any any key to continue!\n\n"
+        STDIN.getch  #Waits for user input (Any Key)
+        puts `clear` # Clears screen after
      #end
     end
 
-     def self.get_score #This is not working at the moment as well
-        player_show = @player_score.to_s
-        computer_show = @computer_score.to_s
-        puts "SCORE: Player - #{player_show} Computer - #{computer_show}"
-        self
-     end
-    
-
+  
 end
 
+#test
+Player.main_player
 newgame = GameRound.new.play
 
-newgame.get_score
+
+#newgame.get_score
 
 
 
