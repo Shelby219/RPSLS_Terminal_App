f01df67d6856887c6aeae2df9597d48c95e96819
ShelbyE <60080830+Shelby219@users.noreply.github.com>
2020-04-27 09:35:48 +1000

    Refactored to house the methods in a module function for clean code
    This is the make it simpler to call these methods in main.rb

--- src/intro.rb
+++ src/intro.rb
@@ -8,47 +8,49 @@ require 'colorize'
 
 
 #WELCOME BANNER FOR THE GAME
-@cursor = TTY::Cursor
-@size = TTY::Screen.size # => [height, width]
-@height = @size[0]
-@width = @size[1]
+module Welcome
+  module_function
+  @cursor = TTY::Cursor
+  @size = TTY::Screen.size # => [height, width]
+  @height = @size[0]
+  @width = @size[1]
 
-def move_cursor_to_required_coordinates(text)
-  x = (@size[1] - text.length) / 2
-  y = (@size[0]) / 2
-  print @cursor.move_to(x, y)
+  def move_cursor_to_required_coordinates(text)
+    x = (@size[1] - text.length) / 2
+    y = (@size[0]) / 2
+    print @cursor.move_to(x, y)
+  end
+
+  def centered_text(text)
+    move_cursor_to_required_coordinates(text)
+    puts text
+  end
+
+  def spinner
+    spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :pulse_2)
+    spinner.auto_spin # Automatic animation with default interval
+    sleep(2) # Perform task
+    spinner.stop('Done!') # Stop animation
+    spinner = TTY::Spinner.new
+  end
+
+  def continue
+    puts "Press any any key to continue!".colorize(:magenta)
+    STDIN.getch  #Waits for user input (Any Key)
+    puts `clear` # Clears screen after
+  end
+  
 end
+Welcome::centered_text("WELCOME TO ROCK PAPER SCISSORS SPOCK LIZARD THE TERMINAL GAME")
+Welcome::spinner
+Welcome::continue
 
-def centered_text(text)
-  move_cursor_to_required_coordinates(text)
-  puts text
-end
-
-centered_text("WELCOME TO ROCK PAPER SCISSORS SPOCK LIZARD THE TERMINAL GAME")
-
-
-#Spinning loading bar
-spinner = TTY::Spinner.new("[:spinner] Loading ...", format: :pulse_2)
-
-spinner.auto_spin # Automatic animation with default interval
-
-sleep(2) # Perform task
-
-spinner.stop('Done!') # Stop animation
-
-spinner = TTY::Spinner.new
-
-puts "Press any any key to continue!".colorize(:magenta)
-
-STDIN.getch  #Waits for user input (Any Key)
-puts `clear` # Clears screen after
 
 #..........................................................................................
 
 #INSTRUCTIONS
 
 def instructions
-
   puts "Made famous by the made famous by the Big Bang Theory, this is ROCK PAPER SCISSORS SPOCK LIZARD!\n\n".colorize(:green)
   puts "It is you against the computer. Try to beat the computers reign as the ultimate RPSSL CHAMPION!\n\n".colorize(:yellow)
   puts "The rules are simple:\n\n".colorize(:yellow)
