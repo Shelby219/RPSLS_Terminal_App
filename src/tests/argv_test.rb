000c63dfe75363abaea53db93b3cbb5a810c4813
Shelby El-rassi <60080830+Shelby219@users.noreply.github.com>
2020-04-27 15:31:08 +1000

    Test file update

---
+++ src/tests/argv_test.rb
@@ -0,0 +1,7 @@
+ARGV.each do|a|
+    puts "Argument: #{a}"
+  end
+
+# ruby argv_test.rb "A" "B"
+# Argument: A
+# Argument: B
---
+++ src/tests/computer_player_test.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+require 'test/unit'
+require_relative '../computer_player.rb'
+
+3
+class ComputerPlayerTest < Test::Unit::TestCase
+  def test_computer_sample_weapon
+    assert_include(Weapons::COMPUTER_CHOICES, :ROCK, message = nil)
+  end
+
+  def test_; end
+end
